ea60ad0cfdde9517d65cf663be2fe7d8a02b37d0
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-07-19 14:30:24 +0200

    export fishier person Controler

--- src/controller/person.ts
+++ src/controller/person.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { personRepository } from "../repository/person-repository";
 
-  const personController = Router();
+export const personController = Router();
 
  personController.get('/', async(req,res)=> {
      const person = await personRepository.findAll();
